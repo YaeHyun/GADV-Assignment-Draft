3f140996b187325ebdaf9fc29a0b3cbde0aa3b08
Izh <izhaqaffandikwon1@gmail.com>
2026-07-01 17:38:58 +0800

    Player Movement
    Player movement and jumping was implemented.

---
+++ Assets/Scripts/Movement Script.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float jumpForce = 0.01f;
+
+    private float moveInput = 0f;
+
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer spriteRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.aKey.isPressed)
+        {
+            moveInput = -1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            moveInput = 1f;
+        }
+        else
+        {
+            moveInput = 0f;
+        }
+
+        rb.linearVelocity = new Vector2(moveInput * movementSpeed, rb.linearVelocity.y);
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+            boxCollider.bounds.center, 
+            boxCollider.bounds.size, 
+            0f, 
+            Vector2.down, 
+            0.1f, 
+            groundLayer
+        );
+        return raycastHit.collider != null;
+    }
+}
